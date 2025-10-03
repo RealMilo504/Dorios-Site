37d33b5e79264a3f8ad1e854ffc32ede7536f8e8
Dorios Studios <chavez100577@gmail.com>
2025-10-03 14:29:51 -0600

    Yes

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'Dorios Studios', // Usually your GitHub org/user name.
+  organizationName: 'RealMilo504', // Usually your GitHub org/user name.
   projectName: 'Dorios-Site', // Usually your repo name.
 
   onBrokenLinks: 'throw',
